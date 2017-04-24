c81488c96c6f3dfc78dd2eaa799e812681e8a680
Marlon Supetran <marlons@campaigntrack.com>
2017-04-24 16:18:29 +0800

    Implemented singleton dbcontext.

---
+++ App_Start/ConfigureDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Realtair.Framework.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Web;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Realtair.Framework.Core.Web.Utilities.ConfigureDbContext), "Start")]
+namespace Realtair.Framework.Core.Web.Utilities
+{
+    public static class ConfigureDbContext
+    {
+        public static void Start()
+        {
+            SingletonDbContext.Instance = new Singleton();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,30 @@
+﻿using Realtair.Framework.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Realtair.Framework.Core.Web
+{
+    public class Singleton : ISingletonDbContext
+    {
+        public DbContext DbContext
+        {
+            get
+            {
+                if (!HttpContext.Current.Items.Contains("dbContext"))
+                {
+                    var a = ConfigurationManager.AppSettings["CoreAssembly"];
+                    var c = ConfigurationManager.AppSettings["CoreContext"];
+
+                    HttpContext.Current.Items.Add("dbContext", (DbContext)Activator.CreateInstance(a, c).Unwrap());
+                }
+                return HttpContext.Current.Items["dbContext"] as DbContext;
+            }
+        }
+    }
+}
