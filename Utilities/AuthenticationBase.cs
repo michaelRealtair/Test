f85dec13062d5825f3cb8f8be9afa20e3926acb0
marlons <marlons@campaigntrack.com>
2017-07-03 18:22:30 +0800

    Re-created SSO implementation of IAuthentication.

---
+++ Utilities/AuthenticationBase.cs
@@ -0,0 +1,55 @@
+﻿using Realtair.Framework.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Realtair.Framework.Core.Entities;
+using System.Web;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Realtair.Framework.Core.Web.Utilities
+{
+    public abstract class AuthenticationBase : IAuthentication
+    {
+        // Fields
+        private ClaimsPrincipal claimsPrincipal;
+        private HttpResponseBase response;
+        private HttpRequestBase request;
+
+        // Properties
+        protected ClaimsPrincipal ClaimsPrincipal => claimsPrincipal;
+        protected HttpResponseBase Response => response;
+        protected HttpRequestBase Request => request;
+        public bool IsLoggedIn => GetUserID() != -1;
+        public int LoggedInUserId => GetUserID();
+
+        // Constructors
+        public AuthenticationBase(HttpResponseBase response, HttpRequestBase request)
+        {
+            this.response = response;
+            this.request = request;
+        }
+        public AuthenticationBase(HttpResponseBase response, HttpRequestBase request, IPrincipal user) 
+            : this(response, request)
+        {
+            claimsPrincipal = user as ClaimsPrincipal;
+        }
+
+        // Method
+        public abstract void LogIn(User user);
+        public abstract void LogOut();
+        protected abstract int GetUserID();
+        protected virtual void ClearCookies()
+        {
+            // Clear all cookies...
+            foreach (var key in request.Cookies.AllKeys)
+            {
+                var cookie = new HttpCookie(key);
+                cookie.Expires = DateTime.Now.AddDays(-1d);
+                response.Cookies.Add(cookie);
+            }
+        }
+    }
+}
---
+++ Utilities/SingleSignOnAuthentication.cs
@@ -0,0 +1,53 @@
+﻿using Realtair.Framework.Core.Entities;
+using Realtair.Framework.Core.IdentityServer.Utilities;
+using Realtair.Framework.Core.Interfaces;
+using System;
+using System.Security.Principal;
+using System.Web;
+
+namespace Realtair.Framework.Core.Web.Utilities
+{
+    public class SingleSignOnAuthentication : AuthenticationBase, IAuthentication
+    {
+        // Constructors
+        public SingleSignOnAuthentication(HttpResponseBase response, HttpRequestBase request)
+            : base(response, request)
+        {
+
+        }
+        public SingleSignOnAuthentication(HttpResponseBase response, HttpRequestBase request, IPrincipal user)
+            : base(response, request, user)
+        {
+        }
+
+        // Methods
+        public override void LogIn(User user)
+        {
+            // Not required when using single sign on...
+        }
+        public override void LogOut()
+        {
+            this.ClearCookies();
+            Request.GetOwinContext().Authentication.SignOut();
+        }
+        protected override int GetUserID()
+        {
+            var sub = ClaimsPrincipal?.Get("sub")?.Value;
+            var email = ClaimsPrincipal?.Get("email")?.Value;
+            var expiry = ClaimsPrincipal?.Get("expires_at")?.Value;
+
+            // Check if expiry date is included, if it has expired or not, and that id is a number...
+            int userId;
+            DateTime expiryDate;
+            if (string.IsNullOrEmpty(sub) ||
+                string.IsNullOrEmpty(email) ||
+                (DateTime.TryParse(expiry, out expiryDate) && expiryDate <= DateTime.UtcNow) ||
+                !int.TryParse(sub, out userId))
+            {
+                userId = -1;
+            }
+
+            return userId;
+        }
+    }
+}
