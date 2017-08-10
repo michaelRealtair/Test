be424ef02a71bb19760bc94da281af1cb41bbc2c
marlons <marlons@campaigntrack.com>
2017-08-10 15:34:51 +0800

    Added error controller.

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace Realtair.Framework.Core.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("error")]
+        [AllowAnonymous]
+        public ActionResult Index(string errorType)
+        {
+            return View();
+        }
+
+        [Route("error/{errorType}")]
+        [AllowAnonymous]
+        public ActionResult HandleError(string errorType)
+        {
+            return View(errorType);
+        }
+    }
+}
