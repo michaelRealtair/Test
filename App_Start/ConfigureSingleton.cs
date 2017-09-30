9aa6744651f54afa3ebbfe96247bc99e4943d571
marlons <marlons@campaigntrack.com>
2017-09-30 18:51:46 +0800

    Implemented assembly cache using singleton.

--- App_Start/ConfigureSingleton.cs
+++ App_Start/ConfigureSingleton.cs
@@ -1,4 +1,5 @@
 ﻿using Realtair.Framework.Core.Data;
+using Realtair.Framework.Core.Reflection;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Realtair.Framework.Core.Web.Utilities.ConfigureSingleton), "Start")]
 namespace Realtair.Framework.Core.Web.Utilities
@@ -7,7 +8,9 @@ namespace Realtair.Framework.Core.Web.Utilities
     {
         public static void Start()
         {
-            SingletonDbContext.Instance = new Singleton();
+            var singleton = new Singleton();
+            SingletonDbContext.Instance = singleton;
+            SingletonAssemblyCache.Instance = singleton;
         }
     }
 }
--- Singleton.cs
+++ Singleton.cs
@@ -1,41 +1,81 @@
 ﻿using Realtair.Framework.Core.Data;
+using Realtair.Framework.Core.Reflection;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Web;
 
 namespace Realtair.Framework.Core.Web
 {
-    public class Singleton : ISingletonDbContext
+    public class Singleton : ISingletonDbContext, ISingletonAssemblyCache
     {
-        static DbContext dbContext;
+        // Constant Fields
+        private const string KEY_DB_CONTEXT = "DB_CONTEXT";
+        private const string KEY_ASSEMBLY_CACHE = "ASSEMBLY_CACHE";
+
+        // Fields
+        private static string coreAssembly = ConfigurationManager.AppSettings["CoreAssembly"];
+        private static string coreContext = ConfigurationManager.AppSettings["CoreContext"];
+        private static DbContext _dbContext;
+        private static AssemblyCache _assemblyCache;
+
+        // Properties
         public DbContext DbContext
         {
             get
             {
-                var a = ConfigurationManager.AppSettings["CoreAssembly"];
-                var c = ConfigurationManager.AppSettings["CoreContext"];
-         
+                var dbContext = Activator
+                    .CreateInstance(coreAssembly, coreContext)
+                    .Unwrap() as DbContext;
+
                 if (HttpContext.Current != null)
                 {
-                    if (!HttpContext.Current.Items.Contains("dbContext"))
-                    {                        
-
-                        HttpContext.Current.Items.Add("dbContext", (DbContext)Activator.CreateInstance(a, c).Unwrap());
-                    }
-                    return HttpContext.Current.Items["dbContext"] as DbContext;
+                    return SetHttpContextItem(KEY_DB_CONTEXT, dbContext);
                 }
                 else
-                {                    
-                    if (dbContext == null) 
-                        dbContext = (DbContext)Activator.CreateInstance(a, c).Unwrap();
-                    return dbContext;
+                {
+                    if (_dbContext == null)
+                        _dbContext = dbContext;
+                    return _dbContext;
                 }                
             }
+        }
+        public AssemblyCache AssemblyCache
+        {
+            get
+            {
+                var assemblyCache = new AssemblyCache(Assembly.Load(coreAssembly));
+
+                if (HttpContext.Current != null)
+                {
+                    return SetHttpContextItem(KEY_ASSEMBLY_CACHE, assemblyCache);
+                }
+                else
+                {
+                    if (_assemblyCache == null)
+                        _assemblyCache = assemblyCache;
+
+                    return _assemblyCache;
+                }
+            }
+        }
+
+        // Methods
+        private T SetHttpContextItem<T>(string key, T t) where T : class
+        {
+            T value = null;
+            var context = HttpContext.Current;
+
+            if (context != null)
+            {
+                if (!context.Items.Contains(key))
+                    context.Items.Add(key, t);
+
+                 value = context.Items[key] as T;
+            }
+
+            return value;
         }
     }
 }
