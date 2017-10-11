aab363c32522485b9f8535748a0db4c0f43f05fe
marlons <marlons@campaigntrack.com>
2017-10-11 19:33:02 +0800

    RT-112: Updated SearchController so that the ISearchable<> declared at the topmost level (the child classes) is fetched first before the inherited ones (the ones implemented by the parent class).

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -13,31 +13,49 @@ namespace Realtair.Framework.Core.Web.Controllers
 {
     public class SearchController : BaseController
     {
+        // Constructors
         public SearchController(IAuthenticationFactory authenticationFactory)
             : base(authenticationFactory)
         {
         }
 
+        // Private Methods
+        private ISearchProvider GetProvider(string entityTypeName)
+        {
+            var entityType = CoreExtensions.GetEntityType(entityTypeName);
+            var types = entityType.GetInterfaces();
+            var providerType = (
+                // Get the non-inherited provider types first...
+                types
+                    .Except(entityType.BaseType.GetInterfaces())
+                    .FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ISearchable<>))     
+                ??
+                // If there are no non-inherited provider types, get the inherited ones...
+                types.FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ISearchable<>))
+            ).GetGenericArguments()[0];
+
+            return (ISearchProvider)Activator.CreateInstance(providerType);
+        }
+        private SearchResults GetSearchResults(ISearchProvider provider, string query, string text)
+        {
+            return provider.Search(DbContext, User, query, text);
+        }
+        
+        #region Routed Methods
         //[Route("{entityTypeName}/search")]
         public ActionResult Search(string entityTypeName, string query, bool allowMultipleSelection = false, string text = "")
         {
+            var provider = GetProvider(entityTypeName);
+            var searchResults = GetSearchResults(provider, query, text);
+
             if (Request.HttpMethod == "GET")
             {
-                var providerType = CoreExtensions.GetEntityType(entityTypeName).GetInterfaces().First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ISearchable<>)).GetGenericArguments()[0];
-                var provider = (ISearchProvider)Activator.CreateInstance(providerType);
-                var searchResults = provider.Search(DbContext, User, query, text);
                 var model = new SearchViewModel { Provider = provider, Results = searchResults };
-
                 return View(model);
             }
             else
             {
-                var providerType = CoreExtensions.GetEntityType(entityTypeName).GetInterfaces().Last(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ISearchable<>)).GetGenericArguments()[0];
-                var provider = (ISearchProvider)Activator.CreateInstance(providerType);
-
-                var searchResults = provider.Search(DbContext, User, query, text);
                 searchResults.AllowMultipleSelection = allowMultipleSelection;
-                
                 return View("Results", searchResults);
             }
         }
@@ -45,10 +63,8 @@ namespace Realtair.Framework.Core.Web.Controllers
         //[Route("{entityTypeName}/search"), HttpPost]
         public ActionResult Results(string entityTypeName, string query, bool allowMultipleSelection = false, string text = "")
         {
-            var providerType = CoreExtensions.GetEntityType(entityTypeName).GetInterfaces().Last(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ISearchable<>)).GetGenericArguments()[0];
-            var provider = (ISearchProvider)Activator.CreateInstance(providerType);
-
-            var searchResults = provider.Search(DbContext, User, query, text);
+            var provider = GetProvider(entityTypeName);
+            var searchResults = GetSearchResults(provider, query, text);
             searchResults.AllowMultipleSelection = allowMultipleSelection;
 
             if (!allowMultipleSelection)
@@ -56,11 +72,14 @@ namespace Realtair.Framework.Core.Web.Controllers
             else
                 return View(searchResults);
         }
-
+        #endregion
+        
+        #region Nested Types/Classes
         public class SearchViewModel
         {
             public ISearchProvider Provider { get; set; }
             public SearchResults Results { get; set; }
-        }
+        } 
+        #endregion
     }
 }
