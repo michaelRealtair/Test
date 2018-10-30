504c42053db21a6c13aed0710d518d9f3ea1c4b4
Marlon Allan P. Supetran <marlon.supetran@outlook.com>
2018-10-30 09:38:57 +0800

    FLOW-42: Initial setup of node and webpack.

---
+++ webpack.dev.js
@@ -0,0 +1,57 @@
+﻿var path = require("path");
+
+var config = {
+  context: path.join(__dirname, '/Scripts'),
+  entry: {
+    app: './index.js'
+  },
+  output: {
+    path: path.join(__dirname, "./Content/js"),
+    filename: "pitch.js",
+    library: 'Realtair'
+  },
+  devServer: {
+    contentBase: __dirname, // `__dirname` is root of the project
+    port: '3003'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-decorators-legacy', 'transform-class-properties', 'transform-object-rest-spread']
+        }
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-decorators-legacy', 'transform-class-properties', 'transform-object-rest-spread']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'url-loader'
+          },
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
+};
+
+module.exports = config;
---
+++ webpack.prod.js
@@ -0,0 +1,53 @@
+﻿var path = require("path");
+
+var config = {
+  entry: [
+    "babel-polyfill",
+    "./Scripts/index"
+  ],
+  output: {
+    publicPath: "/js/",
+    path: path.join(__dirname, "../../../Realtair.Framework/Realtair.Framework.Core.Web/Content/new/js"),
+    filename: "pitch.js",
+    library: 'Realtair'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-decorators-legacy', 'transform-class-properties', 'transform-object-rest-spread']
+        }
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-decorators-legacy', 'transform-class-properties', 'transform-object-rest-spread']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'url-loader'
+          },
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
+};
+
+module.exports = config;
